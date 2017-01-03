34dcd235a809c0fa113698983e22028e65c5716e
skylar <skylar.kong@phz.fi>
2017-01-03 13:40:10 +0200

    added header and footer to the signup custom template

--- custom-template-signup.php
+++ custom-template-signup.php
@@ -8,8 +8,21 @@
  *
  */
 
-get_header(); ?>
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <?php endif; ?>
+    <?php wp_head(); ?>
+</head>
 
-    <div id="app"></div>
+<body>
+<div id="app"></div>
 
-<?php get_footer(); ?>
+<?php wp_footer(); ?>
+</body>
+</html>
